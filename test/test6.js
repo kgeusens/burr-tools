7fba286ae4fd491ce8cf688481083b60d4ce1dcb
Koen Geusens <koen_geusens@hotmail.com>
2023-09-07 14:30:42 +0200

    modularized for use in vue

---
+++ test/test6.js
@@ -0,0 +1,10 @@
+import Module from '../wasm/burr-tools.js'
+import {readFileSync} from "fs"
+// const FS = require("fs");
+// const xmlFile = readFileSync("test.xml", "utf8");
+var mymod = null
+
+Module().then(myModule => {  
+//    mymod = myModule
+    console.log(mymod.solve(xmlFile))
+  });
--- wasm/post.js
+++ wasm/post.js
@@ -1,2 +1,3 @@
 const solve = cwrap('solvePuzzle', 'string', ['string'])
 Module.solve = solve
+
